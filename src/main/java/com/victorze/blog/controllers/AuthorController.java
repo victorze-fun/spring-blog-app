4ff5dc16f24b817df7f0210cbd836b514c9ac38c
Victor <victorze@protonmail.com>
2022-10-01 18:52:35 -0500

    Update the authors view

--- src/main/java/com/victorze/blog/controllers/AuthorController.java
+++ src/main/java/com/victorze/blog/controllers/AuthorController.java
@@ -2,7 +2,6 @@ package com.victorze.blog.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.victorze.blog.repositories.UserRepository;
@@ -10,15 +9,14 @@ import com.victorze.blog.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 
 @Controller
-@RequestMapping("/authors")
 @AllArgsConstructor
 public class AuthorController {
 
     private UserRepository userRepository;
 
-    @GetMapping
+    @GetMapping("/authors")
     public ModelAndView index() {
-        return new ModelAndView("author/index", "users", userRepository.findAll());
+        return new ModelAndView("authors/index", "users", userRepository.findAll());
     }
 
 }
