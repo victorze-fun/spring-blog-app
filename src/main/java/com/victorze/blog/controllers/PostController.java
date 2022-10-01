d6caf791a84f213077a192b7f06283536cb3090e
Victor <victorze@protonmail.com>
2022-10-01 18:56:24 -0500

    List posts

--- src/main/java/com/victorze/blog/controllers/PostController.java
+++ src/main/java/com/victorze/blog/controllers/PostController.java
@@ -4,6 +4,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.victorze.blog.entities.Post;
 import com.victorze.blog.entities.User;
@@ -16,6 +17,11 @@ import lombok.AllArgsConstructor;
 public class PostController {
 
     private PostRepository postRepository;
+
+    @GetMapping("/")
+    public ModelAndView index() {
+        return new ModelAndView("posts/index", "posts", postRepository.findAll());
+    }
 
     @GetMapping("/posts/create")
     public String create() {
