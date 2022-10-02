aa5b259fcbaafdef2d3f2f04d85aac20b166a433
Victor <victorze@protonmail.com>
2022-10-02 09:16:03 -0500

    Refactor flash messages

--- src/main/java/com/victorze/blog/controllers/AuthController.java
+++ src/main/java/com/victorze/blog/controllers/AuthController.java
@@ -32,7 +32,7 @@ public class AuthController {
             return "redirect:/";
         }
         if (error.isPresent()) {
-            model.addAttribute("errorLogin", "El usuario no existe o hubo un error al introducir el usuario y la contraseña");
+            model.addAttribute("error", "El usuario no existe o hubo un error al introducir el usuario y la contraseña.");
         }
         return "auth/login";
     }
